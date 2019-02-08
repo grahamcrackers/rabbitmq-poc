8a57a2a21523a59cbc6decfecede23a93d0a89ee
Graham Rogers <gmrogers04@gmail.com>
2019-02-08 16:15:09 -0600

    move to respective folders

--- 1_HelloWorld/Send/Program.cs
+++ 1_HelloWorld/Send/Program.cs
@@ -12,11 +12,15 @@ class Send
         {
             channel.QueueDeclare(queue: "hello", durable: false, exclusive: false, autoDelete: false, arguments: null);
 
-            string message = "Hello World!";
+            for (int i = 1; i <= 10; i++)
+            {
+            string message = $"message {i}";
             var body = Encoding.UTF8.GetBytes(message);
 
-            channel.BasicPublish(exchange: "", routingKey: "hello", basicProperties: null, body: body);
-            Console.WriteLine(" [x] Sent {0}", message);
+
+                channel.BasicPublish(exchange: "", routingKey: "hello", basicProperties: null, body: body);
+                Console.WriteLine(" [x] Sent {0}", message);
+            }
         }
 
         Console.WriteLine(" Press [enter] to exit.");
